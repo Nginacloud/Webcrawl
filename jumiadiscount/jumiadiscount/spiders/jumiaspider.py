d1ddcc65aa08ca382bae36c1e24b62b2516b9829
Nginacloud <nginadorcas451@gmail.com>
2024-07-19 13:17:28 +0300

    selenium

--- jumiadiscount/jumiadiscount/spiders/jumiaspider.py
+++ jumiadiscount/jumiadiscount/spiders/jumiaspider.py
@@ -7,4 +7,11 @@ class JumiaspiderSpider(scrapy.Spider):
     start_urls = ["https://www.jumia.co.ke/"]
 
     def parse(self, response):
-        pass
+        for products in response.css('div.itm.col'):
+            yield{
+                'name': products.css('div.name::text').get(),
+                'original_price': products.css('div.prc::attr(data-oprc)').get(),
+                'discount_percentage': products.css('div.bdg._dsct').get(),
+                'discounted_price': products.css('div.prc::text').get(),
+                'url': products.css('a.core::attr(href)').get(),
+            }
